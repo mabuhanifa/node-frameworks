54a6e4edf09a322ac7d252892c89696d898651f4
shourovahmd <moh.abuhanifa@gmail.com>
2022-09-27 03:04:24 +0600

    single user

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,6 +1,13 @@
 const users = require("../users.json");
+
 const getUsers = (req, res) => {
   res.send(users);
 };
 
-module.exports = { getUsers };
+const getUser = (req, res) => {
+  const { id } = req.params;
+  const user = users.find((u) => u.id === Number(id));
+  res.send(user);
+};
+
+module.exports = { getUsers, getUser };
--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const { getUsers } = require("../controllers/usersController");
+const { getUsers, getUser } = require("../controllers/usersController");
 
 const router = express.Router();
 
 router.route("/").get(getUsers);
+
+router.route("/:id").get(getUser);
 
 module.exports = router;
