d8cb7c2887a15319d39db5d01969e05b9d2b3c73
shourovahmd <moh.abuhanifa@gmail.com>
2022-09-27 03:40:59 +0600

    updateUser

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -21,4 +21,17 @@ const deleteUser = (req, res) => {
   res.send(users);
 };
 
-module.exports = { getUsers, getUser, createUser, deleteUser };
+const updateUser = (req, res) => {
+  const { id } = req.params;
+  const user = users.find((u) => u.id === Number(id));
+  const body = req.body;
+  user.name = body.name;
+  user.id = id ? Number(id) : Number(user.id);
+  user.name = body.name ? body.name : user.name;
+  user.gender = body.gender ? body.gender : user.gender;
+  user.contact = body.contact ? body.contact : user.contact;
+  user.address = body.address ? body.address : user.address;
+  user.photoUrl = body.photoUrl ? body.photoUrl : user.photoUrl;
+  res.send(user);
+};
+module.exports = { getUsers, getUser, createUser, deleteUser, updateUser };
--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -4,12 +4,13 @@ const {
   getUser,
   createUser,
   deleteUser,
+  updateUser,
 } = require("../controllers/usersController");
 
 const router = express.Router();
 
 router.route("/").get(getUsers).post(createUser);
 
-router.route("/:id").get(getUser).delete(deleteUser);
+router.route("/:id").get(getUser).delete(deleteUser).patch(updateUser);
 
 module.exports = router;
