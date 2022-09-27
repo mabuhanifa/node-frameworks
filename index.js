531f31fd7124b59b3551d60315213054a97cbbda
shourovahmd <moh.abuhanifa@gmail.com>
2022-09-28 02:56:43 +0600

    errorHandler

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const errorHandler = require("./middlewares/errorHandler");
 const usersRouter = require("./routes/usersRoute");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -14,7 +15,15 @@ app.all("*", (req, res) => {
   res.send(`No Route found`);
 });
 
+app.use(errorHandler);
+
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
 
+process.on("unhandledRejection", (error) => {
+  console.log(error.name, error.message);
+  app.close(() => {
+    process.exit(1);
+  });
+});
